94fcef139f6112ad9eecf97cfa350c528153dea6
Bowen Hui <bohuie@gmail.com>
2012-05-27 20:04:02 -0500

    made static pages

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+module ApplicationHelper
+
+  # returns full title on per-page basis
+  def full_title( page_title )
+    base_title = 'Ruby on Rails Sample App'
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewApp::Application.config.secret_token = 'af4e2c8658981f58eb8529900184f1a3c63d99eb49c9e34e5a11a058e3b18c414c93bddb79a67ce65be480acd0ba4fff4ed803ce93c863f3d558450497a46c0f'
