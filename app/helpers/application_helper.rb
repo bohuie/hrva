654bdf674e71566e33e5d1ff55c565bcdb4f26aa
Bowen Hui <bohuie@gmail.com>
2012-05-28 00:48:14 -0500

    hamlized

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # returns full title on per-page basis
   def full_title( page_title )
-    base_title = 'Ruby on Rails Sample App'
+    base_title = 'New App'
     if page_title.empty?
       base_title
     else
