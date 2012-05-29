94ba9160f47a5c03dab02c55678663493be78ced
Bowen Hui <bohuie@gmail.com>
2012-05-29 16:00:43 -0500

    text and labels changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # returns full title on per-page basis
   def full_title( page_title )
-    base_title = 'New App'
+    base_title = 'HRVA'
     if page_title.empty?
       base_title
     else
