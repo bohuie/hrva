52e29aaa610edbbfd436aabf5cb93f0dd3b37c88
Bowen Hui <bohuie@gmail.com>
2012-05-29 16:31:28 -0500

    more static pages content

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ NewApp::Application.routes.draw do
   match '/help',    :to => 'static_pages#help'
   match '/about',   :to => 'static_pages#about'
   match '/contact', :to => 'static_pages#contact'
+  match '/options', :to => 'static_pages#options'
+  match '/experts', :to => 'static_pages#experts'
   
   
   namespace :admin do
