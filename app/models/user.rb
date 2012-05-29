95cc6fe83dde14875760f93ad00aed936113fe9b
Bowen Hui <bohuie@gmail.com>
2012-05-29 17:42:53 -0500

    user account fields customized in sign up and edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   attr_accessor :login
   
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :login, :admin, :active, :first_name, :last_name,
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :login, :admin, :active, :first_name, :last_name, :phone,
                   :address, :country, :province, :city, :organization, :current_role
   
   validates_presence_of :username, :email
