8b83feae94a78b680a7ec7adcc3e19b9ec83dd37
Bowen Hui <bohuie@gmail.com>
2012-06-06 19:04:59 -0500

    instructions page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :login, :admin, :active, :first_name, :last_name, :phone,
                   :address, :country, :province, :city, :organization, :current_role
   
-  validates_presence_of :username, :email
+  validates_presence_of :username, :email, :first_name, :last_name, :organization, :current_role, :country, :province, :city, :phone
   validates_uniqueness_of :username
   
   before_create :set_active
