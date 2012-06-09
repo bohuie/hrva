4d0d28647bd3ec972ce9ca4f1cb7f8304dff95b2
Bowen Hui <bohuie@gmail.com>
2012-06-08 22:50:50 -0500

    select tag for question type

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,8 @@ class Question < ActiveRecord::Base
   belongs_to :section
 
   validates_presence_of :item, :qtype
+
+  def self.qtypes
+    ['one_response'.to_sym, 'many_responses'.to_sym, 'text_response'.to_sym]
+  end
 end
