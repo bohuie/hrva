4ed09f2a63eba1c69546fd69a40d0bfa57090259
Bowen Hui <bohuie@gmail.com>
2012-06-14 13:41:21 -0500

    questionnaire next

--- app/controllers/user/questionnaires/answers_controller.rb
+++ app/controllers/user/questionnaires/answers_controller.rb
@@ -3,8 +3,23 @@ class User::Questionnaires::AnswersController < ApplicationController
   # continues assessment to next unanswered question
   def new 
     @questionnaire = Questionnaire.find params[:questionnaire_id]
-    @question = @questionnaire.next
-    @answer   = @questionnaire.answers.build :question=>@question
+    @question      = @questionnaire.next
+    @answer        = @questionnaire.answers.build :question=>@question
   end
 
+  def create
+    @questionnaire = Questionnaire.find params[:questionnaire_id]
+    @answer        = @questionnaire.answers.new params[:answer]
+    @question      = @answer.question
+
+    if @answer.save
+      redirect_to new_user_questionnaire_answer_path
+    else
+      flash[:error] = @answer.errors.full_messages.to_sentence
+      render 'new'
+    end
+  end
+
+  def edit
+  end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,17 @@
 class Answer < ActiveRecord::Base
-  attr_accessible :response_text, :question
+  attr_accessible :response_text, :question, :question_id
 
   belongs_to :question
   belongs_to :response
   belongs_to :questionnaire
+
+  validates_presence_of :questionnaire_id, :question_id
+  validate :answer_present
+
+private
+  def answer_present
+    errors.add(:base, 'test' )
+    return false
+  end
+
 end
