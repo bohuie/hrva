0b7fcd852eba5f42651050eae03643b2593576bd
Bowen Hui <bohuie@gmail.com>
2012-06-14 16:39:24 -0500

    one questionnaire per user

--- app/controllers/user/questionnaires_controller.rb
+++ app/controllers/user/questionnaires_controller.rb
@@ -1,7 +1,9 @@
 class User::QuestionnairesController < ApplicationController
 
+  before_filter :authenticate_user!
+
   def create
-    @questionnaire = Questionnaire.new params[:questionnaire] 
+    @questionnaire = current_user.questionnaires.last || current_user.questionnaires.build
     if @questionnaire.save
         redirect_to new_user_questionnaire_answer_path( @questionnaire )
       else
