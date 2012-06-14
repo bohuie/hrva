743bd47ca0908055a051c4a9ecf719cc1cc8f9fd
Bowen Hui <bohuie@gmail.com>
2012-06-14 12:04:28 -0500

    likert radio button form

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -5,7 +5,9 @@ class Questionnaire < ActiveRecord::Base
   has_many   :answers
 
   def next
-    @area_questions = Question.all.select{|q| q.section_id == 1}
-    @area_questions.third
+    #@area_questions = Question.all.select{|q| q.section_id == 13}
+    #@area_questions.third
+    @q = Question.all.select{|q| q.item == 'What is the approximate average age of the population in your area?'}
+    @q.first
   end
 end
