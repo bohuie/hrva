f5f2f7f3c67eb675633df777aecb487504aeda48
Bowen Hui <bohuie@gmail.com>
2012-06-14 23:59:59 -0500

    survey nav, question progress

--- app/models/section.rb
+++ app/models/section.rb
@@ -4,4 +4,18 @@ class Section < ActiveRecord::Base
   has_many :questions
 
   validates_presence_of :title
+
+  def self.tab( num )
+    if num == 1
+      Section.all.select{|s| s.title == 'Area Profile'}.first
+    elsif num == 2
+      Section.all.select{|s| s.title == 'Natural Hazards'}.first
+    elsif num == 3
+      Section.all.select{|s| s.title == 'Preparedness'}.first
+    elsif num == 4
+      Section.all.select{|s| s.title == 'Accidental and Human Caused Hazards'}.first
+    else 
+      Section.all.select{|s| s.title == 'Compounded Risks and Hazards'}.first
+    end
+  end
 end
