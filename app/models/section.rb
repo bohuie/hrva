57cda12b3ca2c8a74a7516ecc8967f9052a293ac
Bowen Hui <bohuie@gmail.com>
2012-06-15 20:58:07 -0500

    fixed new sec nav on tabs if prev sec completed

--- app/models/section.rb
+++ app/models/section.rb
@@ -18,4 +18,21 @@ class Section < ActiveRecord::Base
       Section.all.select{|s| s.title == 'Compounded Risks and Hazards'}.first
     end
   end
+  
+  def prev_complete?( num, q )
+    pnum = num - 1;
+    if pnum > 0
+      psec = Section.tab( pnum )
+
+      # check if num questions in psec is same as num answers in psec
+      num_questions = psec.questions.count
+      num_answers   = q.answers.select{|a| a.question.section == psec}.count
+      return num_questions == num_answers
+
+    else
+      # first sec has no prev sec
+      # so it's same as if prev sec is completed for access purposes
+      return true 
+    end
+  end
 end
